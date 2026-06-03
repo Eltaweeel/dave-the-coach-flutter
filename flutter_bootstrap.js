34b4bffd861d21b4c2752c1f7b7776117aa7a8b9
Eltaweeel <Eltaweeel@users.noreply.github.com>
2026-06-03 19:28:57 +0000

    deploy: bb860bb030e89de4174bf98bbc074dd94f741b6e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3102042834" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1666026954" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
